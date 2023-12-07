eeee63d6f66334117285f9ad6bd37f88be59632a
LofiTea <87150148+LofiTea@users.noreply.github.com>
2023-12-07 12:43:01 -0500

    Update BuyerDashboardGUI.java

--- BuyerDashboardGUI.java
+++ BuyerDashboardGUI.java
@@ -5,12 +5,12 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 /**
- * Homework/Project X: ClassName
- * <p>
- * Describe what the assignment is about.
+ * Project 5: BuyerDashboardGUI
+ *
+ * Utilizes GUI to make a menu page for buyers to buy tickets, view history, and more.
  *
  * @author Henry J. Lee, Lab Section L20
- * @version Month XX, 2023
+ * @version November 13, 2023
  */
 
 public class BuyerDashboardGUI extends JComponent implements Runnable {
